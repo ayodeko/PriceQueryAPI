6678635b1ae959fccf0b976823ebd7e3b6cb8b22
ayodeko <akindekoayooluwa@gmail.com>
2024-07-22 07:49:56 +0300

    Implement IPriceChannel (SubscriptionManager.cs) and IDataSource (BinanceDataSource.cs)

---
+++ AmegaPriceQuery.Service/Services/BinanceDataSource.cs
@@ -0,0 +1,150 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+using AmegaPriceQuery.Core.Interfaces;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AmegaPriceQuery.Service.Services;
+
+public class BinanceDataSource : IDataSource
+{
+    private readonly IConfiguration _configuration;
+    private readonly IPriceUtility _priceUtility;
+    private readonly ILogger<BinanceDataSource> _logger;
+    private CancellationTokenSource _cancellationTokenSource;
+    private ClientWebSocket _webSocket;
+
+
+    public BinanceDataSource(IPriceUtility priceUtility, IConfiguration configuration, ILogger<BinanceDataSource> logger, ClientWebSocket clientWebSocket)
+    {
+        _priceUtility = priceUtility;
+        _configuration = configuration;
+        _webSocket = clientWebSocket;
+        _logger = logger;
+    }
+
+    public event Action<string>? OnMessageReceived;
+
+    public async Task DisconnectFromSocketAsync()
+    {
+        try
+        {
+            _cancellationTokenSource.Cancel();
+            await _webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+            _webSocket.Dispose();
+            _logger.LogInformation("Disconnected from Binance WebSocket.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error disconnecting from Binance WebSocket.");
+        }
+    }
+
+    public async Task BeginPriceDataStreamingAsync(string instrument)
+    {
+        await SendSubscriptionMessageAsync(instrument, "SUBSCRIBE");
+    }
+
+    public async Task StopPriceDataStreamingAsync(string instrument)
+    {
+        await SendSubscriptionMessageAsync(instrument, "UNSUBSCRIBE");
+    }
+
+    public async Task ConnectToSocketAsync()
+    {
+        if (_webSocket.State == WebSocketState.Open)
+        {
+            return; // Already connected
+        }
+        _cancellationTokenSource = new CancellationTokenSource();
+        var url = new Uri(_configuration["WebSocketSettings:BinanceWebSocketUrl"] ??
+                          throw new InvalidOperationException("Binance websocket url is null"));
+        try
+        {
+            await _webSocket.ConnectAsync(url, _cancellationTokenSource.Token);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    private async Task SendSubscriptionMessageAsync(string instrument, string action)
+    {
+        var subscriptionMessage = new
+        {
+            method = action,
+            @params = new[] { $"{instrument}@aggTrade" },
+            id = 1
+        };
+
+        var message = JsonConvert.SerializeObject(subscriptionMessage);
+        var bytes = Encoding.UTF8.GetBytes(message);
+
+        try
+        {
+            await _webSocket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true,
+                CancellationToken.None);
+            _logger.LogInformation("Sent {Action} message for {Instrument} to Binance WebSocket.", action, instrument);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error while sending {Action} message for {Instrument}.", action, instrument);
+        }
+    }
+
+    public async Task ReceiveMessageFromSocket(CancellationToken cancellationToken)
+    {
+        var buffer = new byte[1024 * 4];
+        try
+        {
+            while (_webSocket.State == WebSocketState.Open && !cancellationToken.IsCancellationRequested)
+            {
+                var result = await _webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellationToken);
+                switch (result.MessageType)
+                {
+                    case WebSocketMessageType.Text:
+                    {
+                        var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                        Console.WriteLine("Socket Message " + message);
+                        HandleMessage(message);
+                        break;
+                    }
+                    case WebSocketMessageType.Close:
+                        await _webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing",
+                            CancellationToken.None);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error while receiving message from socket.");
+        }
+    }
+
+    private void HandleMessage(string message)
+    {
+        try
+        {
+            var json = JObject.Parse(message);
+
+            // Check if the message is a subscription response
+            if (json["result"] != null && json["id"] != null)
+            {
+                _logger.LogInformation("Received subscription response: {Message}", message);
+            }
+            else
+            {
+                OnMessageReceived?.Invoke(message);
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error handling received message: {Message}", message);
+        }
+    }
+}
---
+++ AmegaPriceQuery.Service/Services/SubscriptionManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Concurrent;
+using AmegaPriceQuery.Core.Interfaces;
+using Newtonsoft.Json.Linq;
+
+namespace AmegaPriceQuery.Service.Services;
+
+public class SubscriptionManager : IPriceChannel
+{
+    private readonly ILogger<SubscriptionManager> _logger;
+    private readonly ConcurrentDictionary<string, int> _subscriptions;
+    private readonly object _lock = new object();
+    private readonly IDataSource _dataSource;
+    private readonly IPriceUtility _priceUtility;
+    
+
+    public SubscriptionManager(IDataSource dataSource, IPriceUtility priceUtility, ILogger<SubscriptionManager> logger)
+    {
+        _dataSource = dataSource;
+        _logger = logger;
+        _subscriptions = new ConcurrentDictionary<string, int>();
+        _priceUtility = priceUtility;
+        _dataSource.OnMessageReceived += BroadcastPrice;
+    }
+
+
+    public async Task SubscribeAsync(string instrument)
+    {
+        instrument = instrument.ToLower();
+        if (_subscriptions.ContainsKey(instrument))
+        {
+            _subscriptions[instrument]++;
+        }
+        else
+        {
+            _subscriptions.TryAdd(instrument, 1);
+            await _dataSource.ConnectToSocketAsync();
+            // Start receiving messages in the background
+            _ = Task.Run((Func<Task>)(async () => await _dataSource.ReceiveMessageFromSocket(new CancellationTokenSource().Token)));
+
+            await _dataSource.BeginPriceDataStreamingAsync(instrument);
+        }
+    }
+
+    public async Task UnsubscribeAsync(string instrument)
+    {
+        instrument = instrument.ToLower();
+        if (_subscriptions.ContainsKey(instrument))
+        {
+            _subscriptions[instrument]--;
+            if (_subscriptions[instrument] == 0)
+            {
+                _subscriptions.TryRemove(instrument, out _);
+                await _dataSource.StopPriceDataStreamingAsync(instrument);
+            }
+        }
+        
+        if (_subscriptions.IsEmpty)
+        {
+            await _dataSource.DisconnectFromSocketAsync();
+        }
+    }
+
+    public void BroadcastPrice(string message)
+    {
+        var response = ParsePrice(message);
+        _priceUtility.UpdatePrice(response.Item1, response.Item2);
+    }
+
+    Tuple<string, decimal> ParsePrice(string message)
+    {
+        try
+        {
+            var json = JObject.Parse(message);
+
+            // Ensure the message is a trade update
+            if (json["e"]?.ToString() != "aggTrade")
+            {
+                return new System.Tuple<string, decimal>("error", 0);
+            }
+
+            var symbol = json["s"].ToString();
+            var price = decimal.Parse(json["p"]?.ToString() ?? "0");
+
+            return new Tuple<string, decimal>(symbol.ToLower(), price);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error encountered during attempt to parse price");
+            return new System.Tuple<string, decimal>("error", 0);
+        }
+    }
+
+}
