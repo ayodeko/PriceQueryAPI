cd60524d213abb5d18478d27ae93fa2d106a744c
ayodeko <akindekoayooluwa@gmail.com>
2024-07-22 07:51:54 +0300

    implement RequestHandler.cs, update middleware

---
+++ AmegaPriceQuery.Service/Services/RequestHandler.cs
@@ -0,0 +1,54 @@
+﻿using AmegaPriceQuery.Core;
+using AmegaPriceQuery.Core.Interfaces;
+
+namespace AmegaPriceQuery.Service.Services;
+
+
+public class RequestHandler(IPriceChannel channel, IPriceUtility utility, ILogger<RequestHandler> logger)
+    : IRequestHandler
+{
+    public async Task<RestResponse> GetInstrumentsRest()
+    {
+        try
+        {
+            var result = await utility.GetInstruments();
+            var response = new RestResponse(result, 
+                "Retrieved", ResponseCode.Successful);
+            return response;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error encountered while getting price");
+            var response = new RestResponse(null, 
+                "An error occured while retrieving the instruments", ResponseCode.Failed);
+            return response;
+        }
+    }
+    public async Task<RestResponse> GetPriceRest(string instrument)
+    {
+        try
+        {
+            var price = await utility.GetPrice(instrument);
+            if (price != null)
+            {
+                
+                var response = new RestResponse(new { instrument, price }, 
+                    "Retrieved", ResponseCode.Successful);
+                return response;
+            }
+            else
+            {
+                await channel.SubscribeAsync(instrument);
+                logger.LogError("Instrument {Instrument} not found", instrument);
+                var response = new RestResponse(null, 
+                    $"Price for {instrument} not found, please try again later", ResponseCode.Failed);
+                return response;
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error encountered while getting price");
+            throw;
+        }
+    }
+}
--- AmegaPriceQueryAPI/Program.cs
+++ AmegaPriceQueryAPI/Program.cs
@@ -1,9 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Net.WebSockets;
+using AmegaPriceQuery.Core.Interfaces;
+using AmegaPriceQuery.Service.Services;
+using Microsoft.AspNetCore.Mvc;
 
+var builder = WebApplication.CreateBuilder(args);
+// Configure logging to use the console
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IPriceChannel, SubscriptionManager>();
+builder.Services.AddSingleton<IDataSource, BinanceDataSource>();
+builder.Services.AddSingleton<IPriceUtility, PriceUtility>();
+builder.Services.AddScoped<IRequestHandler, RequestHandler>();
+builder.Services.AddSingleton<ClientWebSocket>();
 
 var app = builder.Build();
 
@@ -14,12 +26,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.MapGet("api/GetInstruments", ([FromServices] IRequestHandler handler)
+    => handler.GetInstrumentsRest());
 
+app.MapGet("api/GetPrice", ([FromServices] IRequestHandler handler, string instrument)
+    => handler.GetPriceRest(instrument));
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
